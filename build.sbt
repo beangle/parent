362f3e83ec2aa8b309fd777017b3efbe9c28c95e
段体华 <duantihua@gmail.com>
2021-08-24 22:37:05 +0800

    first commit

---
+++ build.sbt
@@ -0,0 +1,29 @@
+import BuildSettings._
+import sbt._
+
+ThisBuild / version := "0.3.0"
+ThisBuild / description := "Beangle Parent."
+ThisBuild / organization := "org.beangle.parent"
+ThisBuild / homepage := Some(url("https://github.com/beangle/parent"))
+ThisBuild / scmInfo := Some(
+  ScmInfo(
+    url("https://github.com/beangle/bom"),
+    "scm:git@github.com:beangle/parent.git"
+  )
+)
+
+ThisBuild / developers := List(
+  Developer(
+    id = "duantihua",
+    name = "Duan Tihua",
+    email = "duantihua@163.com",
+    url = url("https://github.com/duantihua")
+  )
+)
+
+lazy val root = (project in file("."))
+  .enablePlugins(SbtPlugin)
+  .settings(
+    name := "sbt-beangle-parent",
+    commonSettings
+  )
---
+++ src/main/scala/org/beangle/parent/Dependencies.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2005, The Beangle Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.beangle.parent
+
+import sbt.Keys._
+import sbt._
+
+object Dependencies extends sbt.AutoPlugin {
+
+  val slf4j = "org.slf4j" % "slf4j-api" % "2.0.0-alpha4"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.9" % "test"
+  val logback_classic = "ch.qos.logback" % "logback-classic" % "1.3.0-alpha9"
+  val logback_core = "ch.qos.logback" % "logback-core" % "1.3.0-alpha9"
+
+  val apache_commons_compress = "org.apache.commons" % "commons-compress" % "1.21"
+
+  override def trigger = allRequirements
+
+}
