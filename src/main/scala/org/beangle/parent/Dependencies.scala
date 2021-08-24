c9772c0be44de771c92b1d7a1f1197398ae32030
段体华 <duantihua@gmail.com>
2021-08-25 00:05:12 +0800

    add freemarker

--- src/main/scala/org/beangle/parent/Dependencies.scala
+++ src/main/scala/org/beangle/parent/Dependencies.scala
@@ -25,15 +25,17 @@ object Dependencies extends sbt.AutoPlugin {
   val scalaxml = "org.scala-lang.modules" %% "scala-xml" % "2.0.1"
 
   val slf4j = "org.slf4j" % "slf4j-api" % "2.0.0-alpha4"
-  val logback_classic = "ch.qos.logback" % "logback-classic" % "1.3.0-alpha9"
-  val logback_core = "ch.qos.logback" % "logback-core" % "1.3.0-alpha9"
   val apache_commons_compress = "org.apache.commons" % "commons-compress" % "1.21"
 
   val spring_beans ="org.springframework" % "spring-beans" % "5.3.6"
   val spring_context ="org.springframework" % "spring-context" % "5.3.6"
-
   val servletapi = "jakarta.servlet" % "jakarta.servlet-api" % "5.0.0"
+  var freemarker = "org.freemarker" % "freemarker" % "2.3.31"
+  //runtime
+  val logback_classic = "ch.qos.logback" % "logback-classic" % "1.3.0-alpha9"  % "provided"
+  val logback_core = "ch.qos.logback" % "logback-core" % "1.3.0-alpha9"  % "provided"
 
+  //test
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.9" % "test"
   val mockito = "org.mockito" % "mockito-core" % "3.11.1" % "test"
   val h2 ="com.h2database" %"h2"% "1.4.200" % "test"
