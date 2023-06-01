1948b847969aa1f6479a819e3d73d436d4549398
段体华 <duantihua@gmail.com>
2023-06-02 00:33:46 +0800

    Update to scala 3.3.0   Dependencies:     hibernate 6.2.2.Final -> 6.2.3.Final     spring          6.0.8 -> 6.0.9     protobuf-java  3.22.3 -> 3.23.2     poi             4.1.2 -> 5.2.3     bcprov-jdk15to18 1.72 -> 1.73     jedis           4.3.2 -> 4.4.1     undertow  2.3.5.Final -> 2.3.6.Final     tomcat-embed-core 10.1.7 -> 10.1.9

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 import BuildSettings._
 import sbt._
 
-ThisBuild / version := "0.8.8-SNAPSHOT"
+ThisBuild / version := "0.9.0"
 ThisBuild / description := "Beangle Parent."
 ThisBuild / organization := "org.beangle.parent"
 ThisBuild / homepage := Some(url("https://github.com/beangle/parent"))
--- src/main/scala/org/beangle/parent/Dependencies.scala
+++ src/main/scala/org/beangle/parent/Dependencies.scala
@@ -17,7 +17,6 @@
 
 package org.beangle.parent
 
-import sbt.Keys._
 import sbt._
 
 object Dependencies extends sbt.AutoPlugin {
@@ -34,14 +33,14 @@ object Dependencies extends sbt.AutoPlugin {
 
   val gson = "com.google.code.gson" % "gson" % "2.10.1"
   //cdi
-  val spring_beans = "org.springframework" % "spring-beans" % "6.0.8"
-  val spring_context = "org.springframework" % "spring-context" % "6.0.8"
-  val spring_tx = "org.springframework" % "spring-tx" % "6.0.8"
-  val spring_aop = "org.springframework" % "spring-aop" % "6.0.8"
-  val spring_jdbc = "org.springframework" % "spring-jdbc" % "6.0.8"
-  val protobuf = "com.google.protobuf" % "protobuf-java" % "3.22.3"
+  val spring_beans = "org.springframework" % "spring-beans" % "6.0.9"
+  val spring_context = "org.springframework" % "spring-context" % "6.0.9"
+  val spring_tx = "org.springframework" % "spring-tx" % "6.0.9"
+  val spring_aop = "org.springframework" % "spring-aop" % "6.0.9"
+  val spring_jdbc = "org.springframework" % "spring-jdbc" % "6.0.9"
+  val protobuf = "com.google.protobuf" % "protobuf-java" % "3.23.2"
 
-  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "6.2.2.Final"
+  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "6.2.3.Final"
   val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "6.2.2.Final" exclude("org.hibernate.orm", "hibernate-core")
   val byte_buddy = "net.bytebuddy" % "byte-buddy" % "1.12.23"
   val javassist = "org.javassist" % "javassist" % "3.29.2-GA"
@@ -50,21 +49,21 @@ object Dependencies extends sbt.AutoPlugin {
   val jna = "net.java.dev.jna" % "jna" % "5.13.0"
 
   //doc
-  val poi = "org.apache.poi" % "poi" % "4.1.2"
-  val poi_ooxml = "org.apache.poi" % "poi-ooxml" % "4.1.2"
+  val poi = "org.apache.poi" % "poi" % "5.2.3"
+  val poi_ooxml = "org.apache.poi" % "poi-ooxml" % "5.2.3"
   val itextpdf = "com.itextpdf" % "itextpdf" % "5.5.13.3"
   val itext_asian = "com.itextpdf" % "itext-asian" % "5.2.0"
   val itext_xmlworker = "com.itextpdf.tool" % "xmlworker" % "5.5.13.3"
-  val bcprov_jdk15to18 = "org.bouncycastle" % "bcprov-jdk15to18" % "1.72"
-  val bcpkix_jdk15to18 = "org.bouncycastle" % "bcpkix-jdk15to18" % "1.72"
+  val bcprov_jdk15to18 = "org.bouncycastle" % "bcprov-jdk15to18" % "1.73"
+  val bcpkix_jdk15to18 = "org.bouncycastle" % "bcpkix-jdk15to18" % "1.73"
 
   //cache
   val ehcache = "org.ehcache" % "ehcache" % "3.10.8"
   val caffeine = "com.github.ben-manes.caffeine" % "caffeine" % "3.1.6"
   val caffeine_jcache = "com.github.ben-manes.caffeine" % "jcache" % "3.1.6" exclude("org.osgi", "org.osgi.service.component.annotations") exclude("javax.inject", "javax.inject")
 
-  val jgroups = "org.jgroups" % "jgroups" % "5.2.13.Final"
-  val jedis = "redis.clients" % "jedis" % "4.3.2"
+  val jgroups = "org.jgroups" % "jgroups" % "5.2.14.Final"
+  val jedis = "redis.clients" % "jedis" % "4.4.1"
 
   //mail
   val jakarta_mail_api = "jakarta.mail" % "jakarta.mail-api" % "2.0.1"
@@ -72,8 +71,8 @@ object Dependencies extends sbt.AutoPlugin {
 
   val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2022.14"
   //test
-  val scalatest = "org.scalatest" %% "scalatest" % "3.2.15" % "test"
-  val mockito = "org.mockito" % "mockito-core" % "5.2.0" % "test"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.16" % "test"
+  val mockito = "org.mockito" % "mockito-core" % "5.3.1" % "test"
   val greenmail = "com.icegreen" % "greenmail" % "2.0.0" % "test"
 
   //runtime
@@ -85,12 +84,12 @@ object Dependencies extends sbt.AutoPlugin {
   val jtds = "net.sourceforge.jtds" % "jtds" % "1.3.1"
   val ojdbc11 = "com.oracle.database.jdbc" % "ojdbc11" % "23.2.0.0"
   val orai18n = "com.oracle.database.nls" % "orai18n" % "23.2.0.0"
-  val mysql_connector_java = "mysql" % "mysql-connector-java" % "8.0.32"
+  val mysql_connector_java = "com.mysql" % "mysql-connector-j" % "8.0.33"
   val mssql_jdbc = "com.microsoft.sqlserver" % "mssql-jdbc" % "12.2.0.jre11"
 
-  val tomcat_embed_core = "org.apache.tomcat.embed" % "tomcat-embed-core" % "10.1.7"
-  val io_undertow_core = "io.undertow" % "undertow-core" % "2.3.5.Final"
-  val io_undertow_servlet = "io.undertow" % "undertow-servlet" % "2.3.5.Final"
+  val tomcat_embed_core = "org.apache.tomcat.embed" % "tomcat-embed-core" % "10.1.9"
+  val io_undertow_core = "io.undertow" % "undertow-core" % "2.3.6.Final"
+  val io_undertow_servlet = "io.undertow" % "undertow-servlet" % "2.3.6.Final"
 
   override def trigger = allRequirements
 
