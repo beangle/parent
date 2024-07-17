11b98abb213acb4a0be4a0f921ebfb45f149e793
段体华 <duantihua@gmail.com>
2024-07-17 18:49:16 +0800

    Update to hibernate 6.4.9 and spring 6.1.11 Dependencies:   spring            6.1.6  -> 6.1.11   commons-jexl3        3.3 -> 3.4.0   poi               5.2.5  -> 5.3.0   itextpdf           8.0.2 -> 8.0.4   jedis              5.1.2 -> 5.1.3   tomcat           10.1.20 -> 10.1.26   mssql-jdbc  12.6.1.jre11 -> 12.7.0   ojdbc11     23.3.0.23.09 -> 23.4.0.24.05   mysql-con*r-j      8.3.0 -> 9.0.0

--- src/main/scala/org/beangle/parent/Dependencies.scala
+++ src/main/scala/org/beangle/parent/Dependencies.scala
@@ -33,15 +33,15 @@ object Dependencies extends sbt.AutoPlugin {
 
   val gson = "com.google.code.gson" % "gson" % "2.10.1"
   //cdi
-  val spring_beans = "org.springframework" % "spring-beans" % "6.1.6"
-  val spring_context = "org.springframework" % "spring-context" % "6.1.6"
-  val spring_tx = "org.springframework" % "spring-tx" % "6.1.6"
-  val spring_aop = "org.springframework" % "spring-aop" % "6.1.6"
-  val spring_jdbc = "org.springframework" % "spring-jdbc" % "6.1.6"
+  val spring_beans = "org.springframework" % "spring-beans" % "6.1.11"
+  val spring_context = "org.springframework" % "spring-context" % "6.1.11"
+  val spring_tx = "org.springframework" % "spring-tx" % "6.1.11"
+  val spring_aop = "org.springframework" % "spring-aop" % "6.1.11"
+  val spring_jdbc = "org.springframework" % "spring-jdbc" % "6.1.11"
   val protobuf = "com.google.protobuf" % "protobuf-java" % "3.25.3"
 
-  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "6.4.4.Final"
-  val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "6.4.4.Final" exclude("org.hibernate.orm", "hibernate-core")
+  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "6.4.9.Final"
+  val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "6.4.9.Final" exclude("org.hibernate.orm", "hibernate-core")
   val byte_buddy = "net.bytebuddy" % "byte-buddy" % "1.14.13"
   val javassist = "org.javassist" % "javassist" % "3.30.2-GA"
   val jpa = "jakarta.persistence" % "jakarta.persistence-api" % "3.1.0"
@@ -49,10 +49,10 @@ object Dependencies extends sbt.AutoPlugin {
   val jna = "net.java.dev.jna" % "jna" % "5.14.0"
 
   //doc
-  val poi = "org.apache.poi" % "poi" % "5.2.5"
-  val poi_ooxml = "org.apache.poi" % "poi-ooxml" % "5.2.5"
-  val itext = "com.itextpdf" % "itext-core" % "8.0.3"
-  val pdfbox = "org.apache.pdfbox" % "pdfbox" % "3.0.1"
+  val poi = "org.apache.poi" % "poi" % "5.3.0"
+  val poi_ooxml = "org.apache.poi" % "poi-ooxml" % "5.3.0"
+  val itext_kernel = "com.itextpdf" % "kernel" % "8.0.4"
+  val pdfbox = "org.apache.pdfbox" % "pdfbox" % "3.0.2"
 
   //cache
   val ehcache = "org.ehcache" % "ehcache" % "3.10.8"
@@ -60,43 +60,43 @@ object Dependencies extends sbt.AutoPlugin {
   val caffeine_jcache = "com.github.ben-manes.caffeine" % "jcache" % "3.1.8" exclude("org.osgi", "org.osgi.service.component.annotations") exclude("javax.inject", "javax.inject")
 
   val jgroups = "org.jgroups" % "jgroups" % "5.3.6.Final"
-  val jedis = "redis.clients" % "jedis" % "5.1.2"
+  val jedis = "redis.clients" % "jedis" % "5.1.3"
   val lettuce = "io.lettuce" % "lettuce-core" % "6.3.2.RELEASE"
 
   // network
-  val netty_common = "io.netty" % "netty-common" % "4.1.107.Final"
-  val netty_handler = "io.netty" % "netty-handler" % "4.1.107.Final"
-  val netty_transport = "io.netty" % "netty-transport" % "4.1.107.Final"
+  val netty_common = "io.netty" % "netty-common" % "4.1.111.Final"
+  val netty_handler = "io.netty" % "netty-handler" % "4.1.111.Final"
+  val netty_transport = "io.netty" % "netty-transport" % "4.1.111.Final"
 
   //mail
   val jakarta_mail_api = "jakarta.mail" % "jakarta.mail-api" % "2.1.3"
   val jakarta_mail_angus = "org.eclipse.angus" % "angus-mail" % "2.0.3"
 
-  val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2024.4"
+  val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2024.6"
   //test
-  val scalatest = "org.scalatest" %% "scalatest" % "3.2.18" % "test"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.2.19" % "test"
   val mockito = "org.mockito" % "mockito-core" % "5.11.0" % "test"
   val greenmail = "com.icegreen" % "greenmail" % "2.1.0-alpha-4" % "test"
 
   //script
-  val aviator = "com.googlecode.aviator" % "aviator" % "5.4.1"
-  val jexl3 = "org.apache.commons" % "commons-jexl3" % "3.3" exclude("commons-logging", "commons-logging")
+  val aviator = "com.googlecode.aviator" % "aviator" % "5.4.3"
+  val jexl3 = "org.apache.commons" % "commons-jexl3" % "3.4.0" exclude("commons-logging", "commons-logging")
 
   //runtime
   val logback_classic = "ch.qos.logback" % "logback-classic" % "1.5.6"
   val logback_core = "ch.qos.logback" % "logback-core" % "1.5.6"
   val postgresql = "org.postgresql" % "postgresql" % "42.7.3"
-  val h2 = "com.h2database" % "h2" % "2.2.224"
+  val h2 = "com.h2database" % "h2" % "2.3.230"
   val HikariCP = "com.zaxxer" % "HikariCP" % "5.1.0"
   val jtds = "net.sourceforge.jtds" % "jtds" % "1.3.1"
-  val ojdbc11 = "com.oracle.database.jdbc" % "ojdbc11" % "23.3.0.23.09"
-  val orai18n = "com.oracle.database.nls" % "orai18n" % "23.3.0.23.09"
-  val mysql_connector_java = "com.mysql" % "mysql-connector-j" % "8.3.0"
-  val mssql_jdbc = "com.microsoft.sqlserver" % "mssql-jdbc" % "12.6.1.jre11"
+  val ojdbc11 = "com.oracle.database.jdbc" % "ojdbc11" % "23.4.0.24.05"
+  val orai18n = "com.oracle.database.nls" % "orai18n" % "23.4.0.24.05"
+  val mysql_connector_java = "com.mysql" % "mysql-connector-j" % "9.0.0"
+  val mssql_jdbc = "com.microsoft.sqlserver" % "mssql-jdbc" % "12.7.0"
 
-  val tomcat_embed_core = "org.apache.tomcat.embed" % "tomcat-embed-core" % "10.1.20"
-  val io_undertow_core = "io.undertow" % "undertow-core" % "2.3.13.Final"
-  val io_undertow_servlet = "io.undertow" % "undertow-servlet" % "2.3.13.Final"
+  val tomcat_embed_core = "org.apache.tomcat.embed" % "tomcat-embed-core" % "10.1.26"
+  val io_undertow_core = "io.undertow" % "undertow-core" % "2.3.15.Final"
+  val io_undertow_servlet = "io.undertow" % "undertow-servlet" % "2.3.15.Final"
 
   override def trigger = allRequirements
 
