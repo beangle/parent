11a6dd58194c48c6f4f44c9ef3f61993a4dc5878
段体华 <duantihua@gmail.com>
2025-07-16 23:59:38 +0800

    update hibernate to 7

--- src/main/scala/org/beangle/parent/Dependencies.scala
+++ src/main/scala/org/beangle/parent/Dependencies.scala
@@ -48,11 +48,11 @@ object Dependencies extends sbt.AutoPlugin {
   val spring_jdbc = "org.springframework" % "spring-jdbc" % "6.2.7"
 
   //jpa
-  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "6.6.17.Final"
-  val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "6.6.17.Final" exclude("org.hibernate.orm", "hibernate-core")
-  val byte_buddy = "net.bytebuddy" % "byte-buddy" % "1.15.11"
+  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "7.0.6.Final"
+  val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "7.0.6.Final" exclude("org.hibernate.orm", "hibernate-core")
+  val byte_buddy = "net.bytebuddy" % "byte-buddy" % "1.17.5"
   val javassist = "org.javassist" % "javassist" % "3.30.2-GA"
-  val jpa = "jakarta.persistence" % "jakarta.persistence-api" % "3.1.0"
+  val jpa = "jakarta.persistence" % "jakarta.persistence-api" % "3.2.0"
 
   //jna
   val jna = "net.java.dev.jna" % "jna" % "5.17.0"
