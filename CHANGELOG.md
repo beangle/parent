687d8ee3502e9094fcbdc74a5fd2809e5ab63305
段体华 <duantihua@gmail.com>
2026-03-11 13:16:18 +0800

    upgrade dependencies (logback, Spring, Hibernate, Netty, etc.)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,19 +2,21 @@
 
 ## Dependencies Update (2025-03-11)
 
-| Dependency | From | To |
-|------------|------|-----|
-| logback (classic, core) | 1.5.29 | 1.5.32 |
-| snakeyaml | 2.5 | 2.6 |
-| typesafe-config | 1.4.5 | 1.4.6 |
-| protobuf-java | 4.33.5 | 4.34.0 |
-| Spring (beans, tx, aop) | 7.0.3 | 7.0.5 |
-| Hibernate (core, jcache) | 7.2.3.Final | 7.2.6.Final |
-| pdfbox | 3.0.6 | 3.0.7 |
-| jedis | 7.2.1 | 7.3.0 |
-| lettuce | 7.1.0 | 7.5.0 |
-| jgroups | 5.5.2.Final | 5.5.3.Final |
+
+| Dependency                         | From        | To          |
+| ---------------------------------- | ----------- | ----------- |
+| logback (classic, core)            | 1.5.29      | 1.5.32      |
+| snakeyaml                          | 2.5         | 2.6         |
+| typesafe-config                    | 1.4.5       | 1.4.6       |
+| protobuf-java                      | 4.33.5      | 4.34.0      |
+| Spring (beans, tx, aop)            | 7.0.3       | 7.0.5       |
+| Hibernate (core, jcache)           | 7.2.3.Final | 7.2.6.Final |
+| pdfbox                             | 3.0.6       | 3.0.7       |
+| jedis                              | 7.2.1       | 7.3.0       |
+| lettuce                            | 7.1.0       | git s7.5.0  |
+| jgroups                            | 5.5.2.Final | 5.5.3.Final |
 | Netty (common, handler, transport) | 4.2.7.Final | 4.2.9.Final |
-| mockito-core | 5.21.0 | 5.22.0 |
+| mockito-core                       | 5.21.0      | 5.22.0      |
+
 
 **Removed:** spring-context, spring-jdbc
--- src/main/scala/org/beangle/parent/Dependencies.scala
+++ src/main/scala/org/beangle/parent/Dependencies.scala
@@ -25,12 +25,12 @@ object Dependencies extends sbt.AutoPlugin {
   val slf4j = "org.slf4j" % "slf4j-api" % "2.0.17"
   val jul_to_slf4j = "org.slf4j" % "jul-to-slf4j" % "2.0.17"
   val log4j_to_slf4j = "org.apache.logging.log4j" % "log4j-to-slf4j" % "2.25.3"
-  val logback_classic = "ch.qos.logback" % "logback-classic" % "1.5.29"
-  val logback_core = "ch.qos.logback" % "logback-core" % "1.5.29"
+  val logback_classic = "ch.qos.logback" % "logback-classic" % "1.5.32"
+  val logback_core = "ch.qos.logback" % "logback-core" % "1.5.32"
 
   //config
-  val snakeyaml = "org.yaml" % "snakeyaml" % "2.5"
-  val typesafe_config = "com.typesafe" % "config" % "1.4.5"
+  val snakeyaml = "org.yaml" % "snakeyaml" % "2.6"
+  val typesafe_config = "com.typesafe" % "config" % "1.4.6"
 
   // file
   val apache_commons_compress = "org.apache.commons" % "commons-compress" % "1.28.0"
@@ -43,18 +43,16 @@ object Dependencies extends sbt.AutoPlugin {
   val java_websocket = "org.java-websocket" % "Java-WebSocket" % "1.6.0"
 
   //serialization
-  val protobuf = "com.google.protobuf" % "protobuf-java" % "4.33.5"
+  val protobuf = "com.google.protobuf" % "protobuf-java" % "4.34.0"
 
   //cdi
-  val spring_beans = "org.springframework" % "spring-beans" % "7.0.3"
-  val spring_context = "org.springframework" % "spring-context" % "7.0.3"
-  val spring_tx = "org.springframework" % "spring-tx" % "7.0.3"
-  val spring_aop = "org.springframework" % "spring-aop" % "7.0.3"
-  val spring_jdbc = "org.springframework" % "spring-jdbc" % "7.0.3"
+  val spring_beans = "org.springframework" % "spring-beans" % "7.0.5"
+  val spring_tx = "org.springframework" % "spring-tx" % "7.0.5"
+  val spring_aop = "org.springframework" % "spring-aop" % "7.0.5"
 
   //jpa
-  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "7.2.3.Final"
-  val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "7.2.3.Final" exclude("org.hibernate.orm", "hibernate-core")
+  val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "7.2.6.Final"
+  val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "7.2.6.Final" exclude("org.hibernate.orm", "hibernate-core")
   val byte_buddy = "net.bytebuddy" % "byte-buddy" % "1.17.8"
   val jpa = "jakarta.persistence" % "jakarta.persistence-api" % "3.2.0"
 
@@ -68,7 +66,7 @@ object Dependencies extends sbt.AutoPlugin {
   val itext_layout = "com.itextpdf" % "layout" % "9.5.0"
   val itext_bouncy_castle_adapter = "com.itextpdf" % "bouncy-castle-adapter" % "9.5.0"
 
-  val pdfbox = "org.apache.pdfbox" % "pdfbox" % "3.0.6"
+  val pdfbox = "org.apache.pdfbox" % "pdfbox" % "3.0.7"
   val jodconverter_local = "org.jodconverter" % "jodconverter-local" % "4.4.11" exclude("org.openoffice", "*")
   val libreoffice = "org.libreoffice" % "libreoffice" % "24.8.4"
 
@@ -76,14 +74,14 @@ object Dependencies extends sbt.AutoPlugin {
   val ehcache = "org.ehcache" % "ehcache" % "3.11.1"
   val caffeine = "com.github.ben-manes.caffeine" % "caffeine" % "3.2.3"
   val caffeine_jcache = "com.github.ben-manes.caffeine" % "jcache" % "3.2.3" exclude("org.osgi", "org.osgi.service.component.annotations") exclude("javax.inject", "javax.inject")
-  val jedis = "redis.clients" % "jedis" % "7.2.1" exclude("com.google.code.gson", "gson") exclude("org.json", "json") exclude("org.slf4j", "*")
-  val lettuce = "io.lettuce" % "lettuce-core" % "7.1.0.RELEASE"
-  val jgroups = "org.jgroups" % "jgroups" % "5.5.2.Final"
+  val jedis = "redis.clients" % "jedis" % "7.3.0" exclude("com.google.code.gson", "gson") exclude("org.json", "json") exclude("org.slf4j", "*")
+  val lettuce = "io.lettuce" % "lettuce-core" % "7.5.0.RELEASE"
+  val jgroups = "org.jgroups" % "jgroups" % "5.5.3.Final"
 
   // network
-  val netty_common = "io.netty" % "netty-common" % "4.2.7.Final"
-  val netty_handler = "io.netty" % "netty-handler" % "4.2.7.Final"
-  val netty_transport = "io.netty" % "netty-transport" % "4.2.7.Final"
+  val netty_common = "io.netty" % "netty-common" % "4.2.9.Final"
+  val netty_handler = "io.netty" % "netty-handler" % "4.2.9.Final"
+  val netty_transport = "io.netty" % "netty-transport" % "4.2.9.Final"
 
   //mail
   val jakarta_mail_api = "jakarta.mail" % "jakarta.mail-api" % "2.1.5"
@@ -92,7 +90,7 @@ object Dependencies extends sbt.AutoPlugin {
   val plantuml = "net.sourceforge.plantuml" % "plantuml" % "1.2025.10"
   //test
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.19" % "test"
-  val mockito = "org.mockito" % "mockito-core" % "5.21.0" % "test"
+  val mockito = "org.mockito" % "mockito-core" % "5.22.0" % "test"
   val greenmail = "com.icegreen" % "greenmail" % "2.1.8" % "test"
 
   //script
